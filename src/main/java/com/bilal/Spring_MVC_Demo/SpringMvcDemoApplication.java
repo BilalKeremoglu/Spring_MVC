0c30135e8a45c7a012711a28498f104c7a4f8170
Bilal Keremoğlu <bilalkeremoglu@windowslive.com>
2018-12-29 00:53:19 +0300

    version1

---
+++ src/main/java/com/bilal/Spring_MVC_Demo/SpringMvcDemoApplication.java
@@ -0,0 +1,14 @@
+package com.bilal.Spring_MVC_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcDemoApplication.class, args);
+	}
+
+}
+
